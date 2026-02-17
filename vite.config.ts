5f49ee04c82925c8c7f944627870274049aaec90
misterxhermit <misterxhermit@gmail.com>
2026-02-16 23:30:20 -0300

    Glyph1.02a

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import { fileURLToPath, URL } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -11,7 +10,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react(), tailwindcss()],
+      plugins: [react()],
       build: {
         rollupOptions: {
           output: {
